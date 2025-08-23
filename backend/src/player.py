1ca7e5d5302ab37c2504ae60dbfdcc27e6700fc0
Ben Salerno <cheesedoodles217@gmail.com>
2025-08-23 15:37:01 -0700

    Making Player into class

--- backend/src/player.py
+++ backend/src/player.py
@@ -2,20 +2,21 @@ from nba_api.stats.static import players
 from nba_api.stats.endpoints import playercareerstats
 import random
 
-def player_points(career) -> int:
-    df = career.get_data_frames()[1].to_dict()
-    return df['PTS'][0]
+class Player:
+    def player_points(self, career) -> int:
+        df = career.get_data_frames()[1].to_dict()
+        return df['PTS'][0]
 
-def player_assists(career) -> int:
-    df = career.get_data_frames()[1].to_dict()
-    return df['AST'][0]
+    def player_assists(self, career) -> int:
+        df = career.get_data_frames()[1].to_dict()
+        return df['AST'][0]
 
-def years_played(career) -> int:
-    df = career.get_data_frames()[0]
-    years_played = len(df)
-    return years_played
+    def years_played(self, career) -> int:
+        df = career.get_data_frames()[0]
+        years_played = len(df)
+        return years_played
 
-def generate_random_player() -> dict:
-    all_players = players.get_players()
-    random_integer = random.randint(0, len(all_players))
-    return all_players[random_integer]
+    def generate_random_player(self) -> dict:
+        all_players = players.get_players()
+        random_integer = random.randint(0, len(all_players))
+        return all_players[random_integer]
